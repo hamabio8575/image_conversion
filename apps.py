671fc811cf52d8b64d23d5b6dd24478c34d9b3ae
sungno <ysn3939@gmail.com>
2024-06-26 14:13:08 +0900

    finally

--- apps.py
+++ apps.py
@@ -54,7 +54,6 @@ for image_number, orginal_path, save_path, keyword, quality, exif_check in path_
             ### exif 데이터 얻기
             exif_bytes = utils.exif_keep(image)
             print(exif_bytes)
-            input()
             ### 테두리
             if border == 'round':
                 resized_image = utils.add_round_corners(image, radius)
